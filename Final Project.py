27021ff139e44b4e8af50488554032518503031d
Jshaw98 <jshaw98@students.kennesaw.edu>
2026-04-27 18:22:21 -0400

    Update Final Project.py
    Added a report option that shows each subject and the total number of hours spent studying.

--- Final Project.py
+++ Final Project.py
@@ -7,19 +7,24 @@ while True:
     print("2. View Study History")
     print("3. Exit")
 
-    choice = input("Enter your choice between 1 and 3")
+    choice = input("Enter your choice between 1 and 3: ")
      
     if choice == "1":
-        subject = input("Enter the subject: ").capaitalize()
+        subject = input("Enter the subject: ").capitalize()
         hours = int(input("Enter study hours: "))
-        study_method = str(input("Enter Style of Studying: "))
-
-        study_history[subject] = hours
+        study_method = input("Enter Style of Studying: ")
         print("Your study session has been entered.")
-          if subject in study_history:
+        
+        if subject in study_history:
             print("You already studied this subject. Updating total hours.")
             study_history[subject] = study_history[subject] + hours
         else:
             study_history[subject] = hours
 
         print("Your study session has been entered.")
+
+    elif choice == "2":
+        print("\nStudy History:")
+        for subject , hours in study_history.items():
+            print(subject, ":", hours, "hours")
+
