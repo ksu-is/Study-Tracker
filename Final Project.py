e92a1cbb96e663f0a81e227c5c4594244980d5a1
Jshaw98 <jshaw98@students.kennesaw.edu>
2026-04-27 17:38:32 -0400

    Update Final Project.py
    Made sure Subjects get capitalized and, updated the study session entering so repeated subjects add to the previous total instead of replacing the old hours.

--- Final Project.py
+++ Final Project.py
@@ -1,6 +1,6 @@
 print("Welcome to Study Tracker")
 
-study_history = []
+study_history = {}
 
 while True: 
     print("\n1. Enter Study Session Stats")
@@ -8,10 +8,18 @@ while True:
     print("3. Exit")
 
     choice = input("Enter your choice between 1 and 3")
-      if choice == "1":
-        subject = input("Enter the subject: ")
+     
+    if choice == "1":
+        subject = input("Enter the subject: ").capaitalize()
         hours = int(input("Enter study hours: "))
         study_method = str(input("Enter Style of Studying: "))
 
         study_history[subject] = hours
+        print("Your study session has been entered.")
+          if subject in study_history:
+            print("You already studied this subject. Updating total hours.")
+            study_history[subject] = study_history[subject] + hours
+        else:
+            study_history[subject] = hours
+
         print("Your study session has been entered.")
